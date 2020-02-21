12e16f82a039c11e5e634f450543d370a2a85cf9
dazun-dtb <61273208+dazun-dtb@users.noreply.github.com>
2020-02-21 13:49:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,7 +7,14 @@ bot = commands.Bot(command_prefix='!')
 token = os.environ['DISCORD_BOT_TOKEN']
 client = discord.Client()
 a=0
-    
+
+@client.event
+async def on_ready():
+    ch_name = "全体報告部屋"
+    for channel in client.get_all_channels():
+        if channel.name == ch_name:
+            await channel.send("起動しました")
+
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
@@ -22,7 +29,3 @@ async def win(ctx):
     await ctx.send("あなたの勝ち数は"+str(a))
 
 bot.run(token)
-
-@client.event
-async def on_ready():
-    await ctx.send('起きたよー')
